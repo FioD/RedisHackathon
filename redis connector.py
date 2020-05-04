bbf60ec2e9d9e035699940471b3fac67dfe45f0b
Fio <fiorella.dotti@gmail.com>
2020-05-04 20:08:25 +0200

    Timeseries with full grafana integration. Now all that remains is having flask pass the data and adding some fake datetime data so that things are visible on the graphs.

--- redis connector.py
+++ redis connector.py
@@ -8,8 +8,6 @@ r = redis.Redis(
     host= "127.0.0.1",
     port= "6379")
 
-r.set("foo","bar")
-r.get("foo")
 
 class BloodPressureReading:
     def __init__(self, min_bp, max_bp):
@@ -17,13 +15,16 @@ class BloodPressureReading:
         self.min_bp = min_bp
         self.max_bp = max_bp
 
+class sp02Reading:
+    def __init__(self, o2):
+        self.time = int(datetime.datetime.now(datetime.timezone.utc).timestamp()*1000)
+        self.oxygen = o2
 
-bp_sample_1 = BloodPressureReading(60, 120)
-print("TIMES")
-print(bp_sample_1.time)
-#time.sleep(0.3)
-bp_sample_2 = BloodPressureReading(50, 110)
-print(bp_sample_2.time)
+class pulseReading:
+    def __init__(self, heartbeat):
+        self.time = int(datetime.datetime.now(datetime.timezone.utc).timestamp()*1000)
+        self.heartbeat = heartbeat
+
 
 
 class Patient:
@@ -50,23 +51,72 @@ class Patient:
             print(element.min_bp, element.max_bp)   
 
 
-p = Patient("Boris")
-p.receive_vitals(bp_sample_1, 98, 100)
-p.get_cur_vitals()
-print("Second Iteration")
-p.receive_vitals(bp_sample_2, 87, 120)
+# p = Patient("Boris")
+# p.receive_vitals(bp_sample_1, 98, 100)
+# p.get_cur_vitals()
+# print("Second Iteration")
+# p.receive_vitals(bp_sample_2, 87, 120)
+
 
 
 #from here on, experiments
 rts = Client()
-rts.delete("blood")
-print("timeseries deleted")
-rts.create('blood', labels={'Time':'Series'})
-print("Timeseries created")
-rts.add("blood", bp_sample_1.time, bp_sample_1.min_bp)
-print("bp1 added, time: ", bp_sample_1.time)
-print("next one: ", bp_sample_2.time)
-rts.add('blood', bp_sample_2.time, bp_sample_2.min_bp)
-print("bp2 added, time: ", bp_sample_2.time)
-print(rts.get("blood"))
 
+
+def delete_previous():
+    rts.delete("blood_min")
+    rts.delete("blood_max")
+    rts.delete("sp02")
+    rts.delete("pulse")
+    print("all previous databases deleted")
+
+def create_all_series():
+    rts.create('blood_min', labels={'Time':'Series'})
+    rts.create('blood_max', labels={'Time':'Series'})
+    rts.create('sp02', labels={'Time':'Series'})
+    rts.create('pulse', labels={'Time':'Series'})
+    print("Timeseries created")
+
+def add_blood_samples():
+    bp_sample_1 = BloodPressureReading(50, 120)
+    time.sleep(1)
+    bp_sample_2 = BloodPressureReading(60, 110)
+    time.sleep(1)
+    bp_sample_3 = BloodPressureReading(70, 130)
+
+    rts.add("blood_min", bp_sample_1.time, bp_sample_1.min_bp)
+    rts.add('blood_min', bp_sample_2.time, bp_sample_2.min_bp)
+    rts.add("blood_min", bp_sample_3.time, bp_sample_3.min_bp)
+    #print(rts.get("blood_min"))
+
+
+    rts.add("blood_max", bp_sample_1.time, bp_sample_1.max_bp)
+    rts.add('blood_max', bp_sample_2.time, bp_sample_2.max_bp)
+    rts.add("blood_max", bp_sample_3.time, bp_sample_3.max_bp)
+    #print(rts.get("blood_max"))
+
+def add_sp02_samples():
+    o2_sample_1 = sp02Reading(98)
+    time.sleep(1)
+    o2_sample_2 = sp02Reading(70)
+    time.sleep(1)
+    o2_sample_3 = sp02Reading(97)
+    rts.add("sp02", o2_sample_1.time, o2_sample_1.oxygen)
+    rts.add("sp02", o2_sample_2.time, o2_sample_2.oxygen)
+    rts.add("sp02", o2_sample_3.time, o2_sample_3.oxygen)
+
+def add_pulse_samples():
+    pulse_sample_1 = pulseReading(98)
+    time.sleep(1)
+    pulse_sample_2 = pulseReading(54)
+    time.sleep(1)
+    pulse_sample_3 = pulseReading(120)
+    rts.add("pulse", pulse_sample_1.time, pulse_sample_1.heartbeat)
+    rts.add("pulse", pulse_sample_2.time, pulse_sample_2.heartbeat)
+    rts.add("pulse", pulse_sample_3.time, pulse_sample_3.heartbeat)
+
+delete_previous()
+create_all_series()
+add_blood_samples()
+add_sp02_samples()
+add_pulse_samples()
